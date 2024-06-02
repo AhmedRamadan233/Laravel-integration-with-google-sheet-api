898ef1d33bbf58e13242566f7887262fd905a69d
Ahmed Ramadan <106644397+AhmedRamadan233@users.noreply.github.com>
2024-06-02 21:15:25 +0300

    now using job but when use route

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\ProcessGoogleSheetsData;
 use App\Models\Order;
 use App\Models\Product;
 use Illuminate\Http\Request;
@@ -9,6 +10,10 @@ use Revolution\Google\Sheets\Facades\Sheets;
 
 class OrderController extends Controller
 {
+    public function processGoogleSheetsData()
+    {
+        ProcessGoogleSheetsData::dispatch();
+    }
 
     public function index()
     {
@@ -33,40 +38,41 @@ class OrderController extends Controller
 
 
 
-    public function processGoogleSheetsData()
-    {
-        $spreadsheetProductId = '14jNcQTMwAlRcx_JJpNszqovWzuBwrjKWvw4eZeotzJw';
-        $productsSheetName = 'products!A2:D6';
-        $products = Sheets::spreadsheet($spreadsheetProductId)
-            ->sheet($productsSheetName)
-            ->get();
+    // public function processGoogleSheetsData()
+    // {
+    //     $spreadsheetProductId = '14jNcQTMwAlRcx_JJpNszqovWzuBwrjKWvw4eZeotzJw';
+    //     $productsSheetName = 'products!A2:D6';
+    //     $products = Sheets::spreadsheet($spreadsheetProductId)
+    //         ->sheet($productsSheetName)
+    //         ->get();
 
-        foreach ($products as $product) {
-            Product::create([
-                'product_name' => $product[0], 
-                'description' => $product[1],
-                'country' => $product[2], 
-                'product_code' => $product[3], 
-            ]);
-        }
+    //     foreach ($products as $product) {
+    //         Product::create([
+    //             'product_name' => $product[0], 
+    //             'description' => $product[1],
+    //             'country' => $product[2], 
+    //             'product_code' => $product[3], 
+    //         ]);
+    //     }
 
-        $spreadsheetId = '1-ftqLjH8-rB2gNRro7uEq8j0j42oGqMvyKOw0v5__bE';
-        $ordersSheetName = 'orders!A2:f6';
-        $orders = Sheets::spreadsheet($spreadsheetId)
-            ->sheet($ordersSheetName)
-            ->get();
+    //     $spreadsheetId = '1-ftqLjH8-rB2gNRro7uEq8j0j42oGqMvyKOw0v5__bE';
+    //     $ordersSheetName = 'orders!A2:f6';
+    //     $orders = Sheets::spreadsheet($spreadsheetId)
+    //         ->sheet($ordersSheetName)
+    //         ->get();
 
-        foreach ($orders as $order) {
-            Order::create([
-                'client_name' => $order[0], 
-                'phone_number' => $order[1],
-                'final_price' => $order[2],
-                'quantity' => $order[3], 
-                'product_id' => $order[4], 
+    //     foreach ($orders as $order) {
+    //         Order::create([
+    //             'client_name' => $order[0], 
+    //             'phone_number' => $order[1],
+    //             'final_price' => $order[2],
+    //             'quantity' => $order[3], 
+    //             'product_id' => $order[4], 
                 
-            ]);
-        }
+    //         ]);
+    //     }
 
-        return response()->json(['message' => 'Google Sheets data processed successfully']);
-    }
+    //     return response()->json(['message' => 'Google Sheets data processed successfully']);
+    // }
+
 }
